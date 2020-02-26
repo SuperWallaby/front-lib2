d4d1f8278b7ec7e50e003d02e0f058849fb95a8a
SuperWallaby <colton950901@gmail.com>
2020-02-26 13:28:22 +0900

    이닛

---
+++ build/__test__/index.spec.js
@@ -0,0 +1,5 @@
+import { sum } from '../index';
+test('sum 4 + 7 = 11', function () {
+    expect(sum(4, 7)).toEqual(11);
+});
+//# sourceMappingURL=index.spec.js.map
---
+++ build/components/MyComponent.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+var MyComponent = function () { return (<div role="heading">My First Component</div>); };
+export default MyComponent;
+//# sourceMappingURL=MyComponent.js.map
